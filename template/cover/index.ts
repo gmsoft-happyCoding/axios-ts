3ab43be272d7887cce232e63aeb641e1c94f73c7
angular-moon <xiaoxuss@163.com>
2019-02-12 14:32:02 +0800

    use typescript

--- template/cover/index.ts
+++ template/cover/index.ts
@@ -1,9 +1,10 @@
 /* eslint-disable */
+import { Opts } from '../Opts.d';
 import instance from './instance';
 import { convertRESTAPI } from '{{$$.relative("util")}}';
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
-function {{$$.convertMethod(mock, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}(opts) {
+function {{$$.convertMethod(mock, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}(opts: Opts) {
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}', opts)<%} else {%> '{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}'<% } %>,
