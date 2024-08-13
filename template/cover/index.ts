b1f7733e940024a047cfc0dbcefa3c13a8b5ab54
和风柒月 <971616154@qq.com>
2024-08-13 10:23:39 +0800

    fix：修正模板参数可选的问题

--- template/cover/index.ts
+++ template/cover/index.ts
@@ -5,7 +5,7 @@ import instance from './instance';
 import { convertRESTAPI } from '{{$$.relative("util")}}';
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
-function {{$$.convertMethod(mock, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}(opts: <% if($$.isREST(mock.url)) {%>WithPathOpts<%} else {%>Opts<% } %>) {
+function {{$$.convertMethod(mock, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}(opts?: <% if($$.isREST(mock.url)) {%>WithPathOpts<%} else {%>Opts<% } %>) {
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}', opts)<%} else {%> '{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}'<% } %>,
